d783556af3adc8c9753e28d0b915704252001d1b
RobReynolds <rob_reynolds_10@hotmail.com>
2016-02-25 14:50:03 -0500

    added new partials. completed contact page. updated page routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Favourite People' });
+  res.render('index', { title: 'My Favourite People'});
+});
+
+/* GET people page. */
+router.get('/people', function(req, res, next) {
+  res.render('index', { title: 'My Favourite People'});
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me'});
 });
 
 module.exports = router;
